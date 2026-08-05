329984091541e043dc919c763e43de3aeae4132a
karthik <karthikeyakagitha4816@gmail.com>
2026-08-05 12:12:37 +0530

    Week 2 Unit Converter

---
+++ Week-02-Python/Day-01-Python-Basics/convert.py
@@ -0,0 +1,29 @@
+#celsius to fahrenheit
+celsius = float(input("celsius: "))
+fahrenheit = celsius*9/5+32
+print("fahrenheit:",fahrenheit)
+
+#kilometers to miles
+kilometers = float(input("kilometers: "))
+miles = kilometers*0.621371
+print("miles:",miles)
+
+#kilograms to pounds
+kilograms = float(input("kilograms: "))
+pounds = kilograms*2.204623
+print("pounds:",pounds)
+
+#fahrenheit to celsius
+fahrenheit = float(input("fahrenheit: "))
+celsius = fahrenheit-32*5/9
+print("celsius:",celsius)
+
+#miles to kilometers
+miles = float(input("miles: "))
+kilometers = miles*1.609344
+print("kilometers:",kilometers)
+
+#pounds to kilograms
+pounds = float(input("pounds: "))
+kilograms = pounds*0.453592
+print("kilograms:",kilograms)
