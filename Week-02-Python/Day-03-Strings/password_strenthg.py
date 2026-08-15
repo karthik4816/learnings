dee2479e550b9979749a30ab7c04758121441a6c
karthik <karthikeyakagitha4816@gmail.com>
2026-08-15 22:05:11 +0530

    Week 2 password strenthg

--- Week-02-Python/Day-03-Strings/password_strenthg.py
+++ Week-02-Python/Day-03-Strings/password_strenthg.py
@@ -6,17 +6,20 @@ has_digit = False
 has_special = False
 
 special_chars = "@#$%&!"
+print("length",len(password))
 
 for ch in password:
 
     if ch.isupper():
         has_upper = True
+        print("has upeer",has_upper) 
     elif ch.islower():
         has_lower = True
     elif ch.isdigit():
         has_digit = True
     elif ch in special_chars:
         has_special = True
+        
 
 if len(password) < 8:
     print("weak password")
